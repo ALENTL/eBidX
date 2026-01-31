bc3f303723aa159a632410261fbc0ad818f41c7e
ALEN T L <alentl5000@gmail.com>
2026-02-01 03:26:05 +0530

    Routing /bid to PlaceBid View

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AuctionList, AuctionDetail
+from .views import AuctionList, AuctionDetail, PlaceBid
 
 urlpatterns = [
     path("auctions/", AuctionList.as_view(), name="auction-list"),
     path("auctions/<int:pk>/", AuctionDetail.as_view(), name="auction-detail"),
+    path("auctions/<int:pk>/bid/", PlaceBid.as_view(), name="place-bid"),
 ]
