e2861d3c54ae16fee870ff617d75b8c6eb226291
ALEN T L <alentl5000@gmail.com>
2026-01-31 15:43:56 +0530

    Using permission classes to make bids

--- backend/auctions/views.py
+++ backend/auctions/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import AuctionItem
 from .serializers import AuctionItemSerializer
 
@@ -6,3 +6,10 @@ from .serializers import AuctionItemSerializer
 class AuctionList(generics.ListCreateAPIView):
     queryset = AuctionItem.objects.all()
     serializer_class = AuctionItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class AuctionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AuctionItem.objects.all()
+    serializer_class = AuctionItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
