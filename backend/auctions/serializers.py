13c9d0f02a64b737b86ede755679560175331862
ALEN T L <alentl5000@gmail.com>
2026-02-01 03:23:36 +0530

    Serializing end_time and current_price

--- backend/auctions/serializers.py
+++ backend/auctions/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from .models import AuctionItem
+from .models import AuctionItem, Bid
 
 
 class AuctionItemSerializer(serializers.ModelSerializer):
     seller = serializers.StringRelatedField()
+    current_price = serializers.SerializerMethodField()
 
     class Meta:
         model = AuctionItem
@@ -12,9 +13,25 @@ class AuctionItemSerializer(serializers.ModelSerializer):
             "title",
             "description",
             "base_price",
-            "condition",
+            "current_price",
             "image",
+            "seller",
+            "condition",
             "is_active",
             "created_at",
-            "seller",
+            "end_date",
         ]
+
+    def get_current_price(self, obj):
+        highest_bid = obj.bids.order_by("-amount").first()
+        if highest_bid:
+            return highest_bid.amount
+        return obj.base_price
+
+
+class BidSerializer(serializers.ModelSerializer):
+    bidder = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Bid
+        fields = ["id", "bidder", "amount", "created_at"]
