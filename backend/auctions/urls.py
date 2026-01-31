7620b161e72542b96c029a9ce0390e2b6631f146
ALEN T L <alentl5000@gmail.com>
2026-01-31 15:44:29 +0530

    Loading auction/<pk> in AuctionDetail

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import AuctionList
+from .views import AuctionList, AuctionDetail
 
-urlpatterns = [path("auctions/", AuctionList.as_view(), name="auction-list")]
+urlpatterns = [
+    path("auctions/", AuctionList.as_view(), name="auction-list"),
+    path("auctions/<int:pk>/", AuctionDetail.as_view(), name="auction-detail"),
+]
