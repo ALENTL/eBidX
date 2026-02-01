8a2e6cf206b4c1317ea553d4dfc491571c0aac83
ALEN T L <alentl5000@gmail.com>
2026-02-02 01:29:43 +0530

    Implementing filters to use search feature

--- backend/auctions/views.py
+++ backend/auctions/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import AuctionItem, Bid
@@ -9,6 +9,9 @@ class AuctionList(generics.ListCreateAPIView):
     queryset = AuctionItem.objects.all()
     serializer_class = AuctionItemSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "description"]
 
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
