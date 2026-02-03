cca84c179d7b30441c2753ccdca027e75021ca5c
ALEN T L <alentl5000@gmail.com>
2026-02-03 22:27:14 +0530

    Changing websocket middleware from AuthMiddlewareStack to TokenAuthMiddleware for async token checking and notification implementation

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from auctions.middleware import TokenAuthMiddleware
 import auctions.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
@@ -18,7 +18,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
+        "websocket": TokenAuthMiddleware(
             URLRouter(auctions.routing.websocket_urlpatterns)
         ),
     }
