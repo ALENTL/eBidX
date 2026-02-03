69a776b6c7869f863f64e8f22a901d343b6f1ac0
ALEN T L <alentl5000@gmail.com>
2026-02-03 15:26:55 +0530

    Removed api/auth as we do have api/login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("auctions.urls")),
-    path("api/auth/", include("dj_rest_auth.urls")),
 ]
 
 if settings.DEBUG:
