67397dea44856c8ba41822f434bc15c8715ec02c
ALEN T L <alentl5000@gmail.com>
2026-02-03 16:21:36 +0530

    Using CustomLoginView to generate token

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from .views import AuctionList, AuctionDetail, PlaceBid, UserDashboard, RegisterUserView
+from .views import (
+    AuctionList,
+    AuctionDetail,
+    PlaceBid,
+    UserDashboard,
+    RegisterUserView,
+    CustomLoginView,
+)
 
 urlpatterns = [
     path("auctions/", AuctionList.as_view(), name="auction-list"),
     path("auctions/<int:pk>/", AuctionDetail.as_view(), name="auction-detail"),
     path("auctions/<int:pk>/bid/", PlaceBid.as_view(), name="place-bid"),
     path("dashboard/", UserDashboard.as_view(), name="user-dashboard"),
-    path("login/", obtain_auth_token, name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),
     path("register/", RegisterUserView.as_view(), name="register"),
 ]
