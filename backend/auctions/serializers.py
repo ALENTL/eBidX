f8312eaf99df3bb562faadbe01b99ad8db56286b
ALEN T L <alentl5000@gmail.com>
2026-02-03 14:50:10 +0530

    RegisterUserSerializer to accept user registration details, validate and append to the database

--- backend/auctions/serializers.py
+++ backend/auctions/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import AuctionItem, Bid, AuctionImage
 
@@ -82,3 +83,25 @@ class BidWithItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
         fields = ["id", "amount", "created_at", "auction"]
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password_confirm"]
+
+    def validate(self, data):
+        if data.get("password") != data.get("password_confirm"):
+            raise serializers.ValidationError({"password": "Passwords do not match!"})
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        return user
