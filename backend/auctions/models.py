9ce7c9f1148773762773675dd2de7db180fa8037
ALEN T L <alentl5000@gmail.com>
2026-02-03 22:20:45 +0530

    Creating notification model to implement notifications

--- backend/auctions/models.py
+++ backend/auctions/models.py
@@ -69,3 +69,19 @@ class WatchList(models.Model):
 
     class Meta:
         unique_together = ("user", "auction")
+
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notifications"
+    )
+    message = models.CharField(max_length=255)
+    link = models.CharField(max_length=255, blank=True, null=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Notification for {self.recipient.username}: {self.message}"
