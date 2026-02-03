36a14e0a60d898e4d824de182630b768e6bd1637
ALEN T L <alentl5000@gmail.com>
2026-02-03 14:50:38 +0530

    Adding login and register path to urlpatterns

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import AuctionList, AuctionDetail, PlaceBid, UserDashboard
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import AuctionList, AuctionDetail, PlaceBid, UserDashboard, RegisterUserView
 
 urlpatterns = [
     path("auctions/", AuctionList.as_view(), name="auction-list"),
     path("auctions/<int:pk>/", AuctionDetail.as_view(), name="auction-detail"),
     path("auctions/<int:pk>/bid/", PlaceBid.as_view(), name="place-bid"),
     path("dashboard/", UserDashboard.as_view(), name="user-dashboard"),
+    path("login/", obtain_auth_token, name="login"),
+    path("register/", RegisterUserView.as_view(), name="register"),
 ]
