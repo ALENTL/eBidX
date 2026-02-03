522bdc72b8ab40c78474c2138710a942cb3b7989
ALEN T L <alentl5000@gmail.com>
2026-02-03 22:23:24 +0530

    Signalling to create notification and push notification to socket and then to user

--- backend/auctions/signals.py
+++ backend/auctions/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-from .models import Bid
+from .models import Bid, Notification
 
 
 @receiver(post_save, sender=Bid)
@@ -18,5 +18,50 @@ def bid_notification(sender, instance, created, **kwargs):
                     "current_price": float(instance.amount),
                     "bidder": instance.bidder.username,
                 },
+            },
+        )
+
+
+@receiver(post_save, sender=Bid)
+def create_user_notifications(sender, instance, created, **kwargs):
+    if created:
+        bid = instance
+        auction = bid.auction
+        bidder = bid.bidder
+
+        if auction.seller != bidder:
+            Notification.objects.create(
+                recipient=auction.seller,
+                message=f"New bid of ₹{bid.amount} on '{auction.title}'",
+                link=f"/auction/{auction.id}",
+            )
+
+        previous_bid = (
+            Bid.objects.filter(auction=auction)
+            .exclude(id=bid.id)
+            .order_by("-amount")
+            .first()
+        )
+
+        if previous_bid and previous_bid.bidder != bidder:
+            Notification.objects.create(
+                recipient=previous_bid.bidder,
+                message=f"[ALERT]You've been outbid on '{auction.title}'!",
+                link=f"/auction/{auction.id}",
+            )
+
+
+@receiver(post_save, sender=Notification)
+def push_notification_socket(sender, instance, created, **kwargs):
+    if created:
+        channel_layer = get_channel_layer()
+        group_name = f"user_{instance.recipient.id}"
+
+        async_to_sync(channel_layer.group_send)(
+            group_name,
+            {
+                "type": "send_notification",
+                "message": instance.message,
+                "link": instance.link,
             },
         )
