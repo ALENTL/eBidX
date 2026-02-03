749c9012da781b3cee6fe8bc3a6c7659c2d78225
ALEN T L <alentl5000@gmail.com>
2026-02-03 22:19:48 +0530

    Defining connect, disconnect and send_notification methods for NotificationConsumer -> send notification to user asynchronously

--- backend/auctions/consumers.py
+++ backend/auctions/consumers.py
@@ -16,3 +16,31 @@ class AuctionConsumer(AsyncWebsocketConsumer):
     async def auction_update(self, event):
         message = event["message"]
         await self.send(text_data=json.dumps({"message": message}))
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope.get("user")
+
+        if not self.user or self.user.is_anonymous:
+            await self.close()
+
+        else:
+            self.group_name = f"user_{self.user.id}"
+
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+    async def disconnect(self, close_code):
+        if self.user and not self.user.is_anonymous:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def send_notification(self, event):
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "type": "notification",
+                    "message": event["message"],
+                    "link": event.get("link", ""),
+                }
+            )
+        )
