7f3382a240e1b01f251af5ea6bbdbe26c781a365
ALEN T L <alentl5000@gmail.com>
2026-02-06 00:34:17 +0530

    Wrapping App in GoogleOAuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
     <App />
-  </StrictMode>,
+  </GoogleOAuthProvider>,
 );
